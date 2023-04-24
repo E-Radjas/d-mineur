2d5043cb8680bf6c12bbb70d5c9d633825c21c8e
clemlad <128020189+clemlad@users.noreply.github.com>
2023-04-24 12:31:53 +0200

    Update démineur.py

--- démineur.py
+++ démineur.py
@@ -5,16 +5,6 @@ NB_MINES = 10
 RANGEES = 10
 COLONNES = 10
 matrice = [[0 for j in range(COLONNES)] for i in range(RANGEES)]
-## matrice = [[0,0,0,0,0,0,0,0,0,0],
-##           [0,0,0,0,0,0,0,0,0,0],
-##           [0,0,0,0,0,0,0,0,0,0],
-##           [0,0,0,0,0,0,0,0,0,0],
-##           [0,0,0,0,0,0,0,0,0,0],
-##           [0,0,0,0,0,0,0,0,0,0],
-##           [0,0,0,0,0,0,0,0,0,0],
-##           [0,0,0,0,0,0,0,0,0,0],
-##           [0,0,0,0,0,0,0,0,0,0]]
-
 for i in range(NB_MINES):
     row = rd.randint(0, RANGEES - 1)
     col = rd.randint(0, COLONNES - 1)
