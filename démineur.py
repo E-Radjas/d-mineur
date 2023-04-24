349401b1fe1e1f0d76473830bb6f82b49e0b8b1d
E-Radjas <80163139+E-Radjas@users.noreply.github.com>
2023-04-24 14:19:51 +0400

    Update démineur.py

--- démineur.py
+++ démineur.py
@@ -1,10 +1,10 @@
-import pygame as pg
 import random as rd
+
 MINES = [rd.randint(0, 9)]
 NB_MINES = 10
-ROWS = 10
-COLS = 10
-matrix = [[0 for j in range(COLS)] for i in range(ROWS)]
+RANGEES = 10
+COLONNES = 10
+matrice = [[0 for j in range(COLONNES)] for i in range(RANGEES)]
 ## matrice = [[0,0,0,0,0,0,0,0,0,0],
 ##           [0,0,0,0,0,0,0,0,0,0],
 ##           [0,0,0,0,0,0,0,0,0,0],
@@ -15,10 +15,12 @@ matrix = [[0 for j in range(COLS)] for i in range(ROWS)]
 ##           [0,0,0,0,0,0,0,0,0,0],
 ##           [0,0,0,0,0,0,0,0,0,0]]
 
-for i in range(NUM_MINES):
-    row = rd.randint(0, ROWS - 1)
-    col = rd.randint(0, COLS - 1)
+for i in range(NB_MINES):
+    row = rd.randint(0, RANGEES - 1)
+    col = rd.randint(0, COLONNES - 1)
     while matrice[row][col] == 9:
-        row = rd.randint(0, ROWS - 1)
-        col = rd.randint(0, COLS - 1)
+        row = rd.randint(0, RANGEES - 1)
+        col = rd.randint(0, COLONNES - 1)
     matrice[row][col] = 9
+
+print(matrice)
