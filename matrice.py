c8e45b480ff1ebbbbe38f374f92d9df5be094e5c
E-Radjas <80163139+E-Radjas@users.noreply.github.com>
2023-05-04 09:56:18 +0400

    Affichage à faire, mais sinon version console finie (juste ajouter interactions avec joueur et affichage vide)

--- matrice.py
+++ matrice.py
@@ -2,28 +2,28 @@ import random as rd
 
 MINES = [rd.randint(0, 9)]
 NB_MINES = 10
-RANGEES = 10
+rangees = 10
 COLONNES = 10
-plateau = [[0 for j in range(COLONNES)] for i in range(RANGEES)]
+plateau = [[0 for j in range(COLONNES)] for i in range(rangees)]
 
 for i in range(NB_MINES):
-    row = rd.randint(0, RANGEES - 1)
+    row = rd.randint(0, rangees - 1)
     col = rd.randint(0, COLONNES - 1)
     while plateau[row][col] == 9:
-        row = rd.randint(0, RANGEES - 1)
+        row = rd.randint(0, rangees - 1)
         col = rd.randint(0, COLONNES - 1)
     plateau[row][col] = 9
 
 # Affichage du cadre
-matrice = [[11 for j in range(COLONNES + 2)] for i in range(RANGEES + 2)]
+matrice = [[11 for j in range(COLONNES + 2)] for i in range(rangees + 2)]
 
 
-for i in range(RANGEES):
+for i in range(rangees):
     for j in range(COLONNES):
         matrice[i+1][j+1] = plateau[i][j]
 
 # nombre de mines autour d'une case
-for i in range(1, RANGEES+1):
+for i in range(1, rangees + 1):
     for j in range(1, COLONNES+1):
         if matrice[i][j] != 9:
             nb_autour = 0
