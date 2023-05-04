aad73514edbb35ca7adbf84fb655f86a8cc9db0b
E-Radjas <80163139+E-Radjas@users.noreply.github.com>
2023-05-04 08:39:21 +0400

    Mines générées avec nombre de mines autour d'une case.

--- matrice.py
+++ matrice.py
@@ -4,14 +4,32 @@ MINES = [rd.randint(0, 9)]
 NB_MINES = 10
 RANGEES = 10
 COLONNES = 10
-matrice = [[0 for j in range(COLONNES)] for i in range(RANGEES)]
+plateau = [[0 for j in range(COLONNES)] for i in range(RANGEES)]
 
 for i in range(NB_MINES):
     row = rd.randint(0, RANGEES - 1)
     col = rd.randint(0, COLONNES - 1)
-    while matrice[row][col] == 9:
+    while plateau[row][col] == 9:
         row = rd.randint(0, RANGEES - 1)
         col = rd.randint(0, COLONNES - 1)
-    matrice[row][col] = 9
-matrice2=[[0 for j in range(COLONNES+2)] for i in range(RANGEES+2)]
+    plateau[row][col] = 9
+
+matrice = [[0 for j in range(COLONNES + 2)] for i in range(RANGEES + 2)]
+
+
+for i in range(RANGEES):
+    for j in range(COLONNES):
+        matrice[i+1][j+1] = plateau[i][j]
+
+# nombre de mines autour d'une case
+for i in range(1, RANGEES+1):
+    for j in range(1, COLONNES+1):
+        if matrice[i][j] != 9:
+            nb_autour = 0
+            for di in [-1, 0, 1]:
+                for dj in [-1, 0, 1]:
+                    if matrice[i+di][j+dj] == 9:
+                        nb_autour += 1
+            matrice[i][j] = nb_autour
+
 print(matrice)
