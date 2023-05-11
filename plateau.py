8aa09706735ef87fe3827f4daa1e3a116c568b7b
E-Radjas <80163139+E-Radjas@users.noreply.github.com>
2023-05-11 09:59:26 +0400

    Ajouté changements de Timeo 05/11/2023
    Version console. Pour affichage graphique voir matrice.py.

--- plateau.py
+++ plateau.py
@@ -1,5 +1,6 @@
 import random as rd
 
+
 MINES = [rd.randint(0, 9)]
 NB_MINES = 10
 RANGEES = 10
@@ -13,88 +14,76 @@ for i in range(NB_MINES):
         row = rd.randint(0, RANGEES - 1)
         col = rd.randint(0, COLONNES - 1)
     plateau[row][col] = 9
-for i in range(row):
-    if i + 1 == 9 or 1 - 1 == 9:
-        i += 1
-for i in range(col):
-    if i + 1 == 9 or 1 - 1 == 9:
-        i += 1
+
 matrice = [[0 for j in range(COLONNES + 2)] for i in range(RANGEES + 2)]
-print(plateau)
 
 
+for i in range(RANGEES):
+    for j in range(COLONNES):
+        matrice[i+1][j+1] = plateau[i][j]
+
+# nombre de mines autour d'une case
+for i in range(1, RANGEES+1):
+    for j in range(1, COLONNES+1):
+        if matrice[i][j] != 9:
+            nb_autour = 0
+            for di in [-1, 0, 1]:
+                for dj in [-1, 0, 1]:
+                    if matrice[i+di][j+dj] == 9:
+                        nb_autour += 1
+            matrice[i][j] = nb_autour
+
+symbol=['0','1','2','3','4','5','6','7','8','X',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','►']
+ligne=0
 
 
-import random
+for x in range(1,11):
+    for b in range(1,11):
+        matrice[x][b]+=10
 
-continuer=1
-# Fonction pour initialiser le plateau de jeu
-while continuer==1:
-    def initialiser_plateau(largeur, hauteur, nb_mines):
-        plateau = [[0 for j in range(largeur)] for i in range(hauteur)]
-        mines_placees = 0
-        while mines_placees < nb_mines:
-            x = random.randint(0, largeur - 1)
-            y = random.randint(0, hauteur - 1)
-            if plateau[y][x] != 'X':
-                plateau[y][x] = 'X'
-                mines_placees += 1
-                for i in range(max(0, y-1), min(hauteur, y+2)):
-                    for j in range(max(0, x-1), min(largeur, x+2)):
-                        if plateau[i][j] != 'X':
-                            plateau[i][j] += 1
-        return plateau
+def affichage():
+    ligne=0
+    print("  1   2   3   4   5   6   7   8   9  10")
+    print("+---+---+---+---+---+---+---+---+---+---+")
+    for i in range(1,11):
+        ligne+=1
+        print("|",symbol[matrice[ligne][1]], "|", symbol[matrice[ligne][2]], "|", symbol[matrice[ligne][3]],"|",symbol[matrice[ligne][4]], "|", symbol[matrice[ligne][5]], "|", symbol[matrice[ligne][6]],"|",symbol[matrice[ligne][7]], "|", symbol[matrice[ligne][8]], "|", symbol[matrice[ligne][9]],"|",symbol[matrice[ligne][10]],"|",ligne)
+        print("+---+---+---+---+---+---+---+---+---+---+")
+def tour():
+    affichage()
+    decouverte()
+    tour()
+    
 
-# Fonction pour afficher le plateau de jeu
-    def afficher_plateau(plateau, decouvert):
-        print('  ' + ' '.join(str(i) for i in range(len(plateau[0]))))
-        for i in range(len(plateau)):
-            ligne = ''
-            for j in range(len(plateau[0])):
-                if decouvert[i][j]:
-                    ligne += str(plateau[i][j])
-                else:
-                    ligne += '-'
-            print(str(i) + ' ' + ligne)
-
-# Fonction pour révéler une case
-    def reveler_case(plateau, decouvert, x, y):
-        if decouvert[y][x]:
-            return decouvert[y][x]==True
-        if plateau[y][x] == 'X':
-            return False
-        if plateau[y][x] == 0:
-            for i in range(max(0, y-1), min(len(plateau), y+2)):
-                for j in range(max(0, x-1), min(len(plateau[0]), x+2)):
-                    reveler_case(plateau, decouvert, j, i)
-            return True
-
-# Fonction pour demander des coordonnées
-    def demander_coordonnees():
-        while True:
-            coordonnees = input('Entrez les coordonnées (x,y) ou q pour quitter : ')
-            if coordonnees.lower() == 'q':
-                return None
-            try:
-                x, y = [int(c) for c in coordonnees.split(',')]
-                if x < 0 or x >= 10 or y < 0 or y >= 10:
-                    print('Coordonnées invalides.')
-                else:
-                    return x, y
-            except:
-                print('Coordonnées invalides.')
-
-# Fonction principale pour jouer au démineur
-    def jouer_demineur():
-        nb_mines = 10
-        plateau = initialiser_plateau(10, 10, nb_mines)
-        decouvert = [[False for j in range(10)] for i in range(10)]
-        partie_terminee = False
-        while not partie_terminee:
-            afficher_plateau(plateau, decouvert)
-            coordonnees = demander_coordonnees()
-            if coordonnees is None:
-                print('Au revoir!')
-                return x,y==coordonnees
-            if not reveler_case(plateau, decouvert, x, y):
-                print('BOUM ! Vous avez perdu')
+    
+def decouverte():
+    coordX = int(input('Entrer la position X de votre sélection'))
+    if 0<coordX<11:
+        coordY = int(input('Entrer la position Y de votre sélection'))
+        if 0<coordY<11:
+            if matrice[coordY][coordX]>9:
+                matrice[coordY][coordX]-=10
+                if matrice[coordY][coordX] == 9:
+                    perdu()
+                    quit()
+                if matrice[coordY][coordX] == 0:
+                    reveler_cases_adjacentes(coordY, coordX)
+            else: tour()
+        else: tour()
+    else: tour()
+def reveler_cases_adjacentes(row, col):
+    for di in [-1, 0, 1]:
+        for dj in [-1, 0, 1]:
+            if matrice[row+di][col+dj] > 9:
+                matrice[row+di][col+dj] -= 10
+                if matrice[row+di][col+dj] == 0:
+                    reveler_cases_adjacentes(row+di, col+dj)
+def perdu():
+    for x1 in range(1, 11):
+        for b1 in range(1, 11):
+            if matrice[x1][b1] == 19:
+                matrice[x1][b1]-=10
+    affichage()
+    print('Vous avez perdu !')
+    quit()
+tour()
